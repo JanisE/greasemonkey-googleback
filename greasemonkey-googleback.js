91ca042205d561b7be36389dcb7544f8f2e663f3
janise <janise@inbox.lv>
2014-03-13 02:10:13 +0200

    - OK, it was not quite tested.

--- greasemonkey-googleback.js
+++ greasemonkey-googleback.js
@@ -8,5 +8,5 @@
 
 var css = document.createElement("style");
 css.type = "text/css";
-css.innerHTML = "#res h3.r a:link {font-size: 16px important; text-decoration: underline !important}";
+css.innerHTML = "#res h3.r a:link {font-size: 16px !important; text-decoration: underline !important}";
 document.head.appendChild(css);
