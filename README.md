e62e5a02397080fe8f3612ae24259178606552af
janise <janise@inbox.lv>
2014-03-13 02:04:32 +0200

    - Initial version.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ greasemonkey-googleback
 =======================
 
 Greasemonkey user script to bring back underlined links in Google search results.
+
+### How to install
+* Install GreaseMonkey: https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/
+* Go to http://userscripts.org/scripts/show/410654 and click on "Install" button, follow instructions.
+
---
+++ greasemonkey-googleback.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name Greasemonkey GoogleBack
+// @namespace https://github.com/JanisE
+// @include *.google.*
+// @version 1
+// @grant none
+// ==/UserScript==
+
+var css = document.createElement("style");
+css.type = "text/css";
+css.innerHTML = "#res h3.r a:link {font-size: 16px important; text-decoration: underline !important}";
+document.head.appendChild(css);
