ba5dbafca80800b7ea0e2f008df02ac738bfc801
janise <janise@inbox.lv>
2014-03-24 19:13:54 +0200

    - Improve the style according to the latest findings of https://github.com/kristapsk/chrome-googleback (ad link style).

--- greasemonkey-googleback.js
+++ greasemonkey-googleback.js
@@ -2,11 +2,12 @@
 // @name Greasemonkey GoogleBack
 // @namespace https://github.com/JanisE
 // @include *.google.*
-// @version 1
+// @version 2
 // @grant none
 // ==/UserScript==
 
 var css = document.createElement("style");
 css.type = "text/css";
-css.innerHTML = "#res h3.r a:link {font-size: 16px !important; text-decoration: underline !important}";
+css.innerHTML = "#res h3.r a, li.ads-ad h3 a {font-size: 16px !important; text-decoration: underline}";
 document.head.appendChild(css);
+
